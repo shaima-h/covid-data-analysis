fd56076c1f3cd1982b822b20564b24252cd4f04b
shaima-h <shaima.hussaini@gmail.com>
2025-01-07 21:27:02 -0500

    death percentage queries

--- covid-data-analysis/Scripts/Script.sql
+++ covid-data-analysis/Scripts/Script.sql
@@ -1,1 +1,11 @@
 SELECT * FROM coviddeaths LIMIT 10;
+
+select location, date, total_cases, new_cases, total_deaths, population
+from coviddeaths
+order by 1,2
+
+-- total cases vs total deaths
+select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as death_percentage
+from coviddeaths
+where location like '%States%'
+order by 1,2;
